361fb4473f13441dd746fdb7f1bbdf924c3d5fad
Mike Foster <i.am.mike.foster@gmail.com>
2023-11-16 13:28:06 +0100

    BugFix: capitalisation error in filename

--- test/majorver/test_filecompare.py
+++ test/majorver/test_filecompare.py
@@ -4,4 +4,4 @@ def test_fileissamesize(duplicatedir1):
     testfiles = duplicatedir1
     filesdict = listfiles(testfiles)
     duplicatefiles = filesofsamesize(filesdict)
-    assert duplicatefiles == {frozenset((testfiles / 'dir1' / 'FileA.txt', testfiles / 'alt' / 'dir1' / 'FileA.txt'))}
+    assert duplicatefiles == {frozenset((testfiles / 'dir1' / 'fileA.txt', testfiles / 'alt' / 'dir1' / 'fileA.txt'))}
