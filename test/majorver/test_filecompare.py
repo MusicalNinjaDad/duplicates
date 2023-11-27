6dd692b42c674c1290bb10ff93a11c0523caaf73
Mike Foster <i.am.mike.foster@gmail.com>
2023-11-27 15:03:47 +0100

    cleaner fixtures working in first cases

--- test/majorver/test_filecompare.py
+++ test/majorver/test_filecompare.py
@@ -1,16 +1,17 @@
 from . import listfiles, filesofsamesize, BufferedIOFile, comparefiles, drophardlinks
 from .testimports import *
 
-def test_fileissamesize(copiedtestfiles, duplicatedir1):
+@mark.copyfiles(('fileA',2))
+def test_fileissamesize(copiedtestfiles):
     filesdict = listfiles(copiedtestfiles.root)
     duplicatefiles = filesofsamesize(filesdict)
-    assert duplicatefiles == {frozenset((copiedtestfiles.paths['fileA'], copiedtestfiles.paths['fileA-copy']))}
+    assert duplicatefiles == {frozenset((copiedtestfiles.paths['fileA'][0], copiedtestfiles.paths['fileA'][1]))}
 
-def test_samefilecontentsfirstchunk(copiedtestfiles, duplicatedir1, fileAopened, fileAcopyopened):
-    filestocompare = frozenset((
-        BufferedIOFile(copiedtestfiles.paths['fileA'], copiedtestfiles.handles['fileA'], chunksize=4),
-        BufferedIOFile(copiedtestfiles.paths['fileA-copy'], copiedtestfiles.handles['fileA-copy'], chunksize=4)
-    ))
+@mark.copyfiles(('fileA',2))
+def test_samefilecontentsfirstchunk(copiedtestfiles, filesopen):
+    filestocompare = frozenset(
+        BufferedIOFile(path_handle[0], path_handle[1], chunksize=4) for path_handle in zip(copiedtestfiles.paths['fileA'], copiedtestfiles.handles['fileA'])
+    )
     identicalfiles = comparefiles(filestocompare)
     assert identicalfiles == {filestocompare}
 
--- test/majorver/testimports.py
+++ test/majorver/testimports.py
@@ -1,14 +1,17 @@
+from collections import defaultdict
+from contextlib import ExitStack
 from dataclasses import dataclass
 from io import BufferedIOBase
 from pathlib import Path
+import uuid
 from pytest import fixture, mark, raises
 
 def _copy(self: Path, target: Path) -> None:
-    from shutil import copytree
+    from shutil import copytree, copyfile
     if self.is_dir():
         copytree(self, target / self.name)
     else:
-        raise NotImplementedError
+        copyfile(self, target / self.name) #If later needed can use newname = self.name: str as optional arg
 
 Path.copy = _copy
 
@@ -21,21 +24,43 @@ class Testfiles():
     paths: dict[str: Path]
     handles: dict[str: BufferedIOBase]
 
+SourceFileRoot = Path('test/majorver/data/')
+sourcefiles = Testfiles(
+    root = SourceFileRoot,
+    paths = {
+        'fileA': SourceFileRoot / 'dir1' / 'fileA.txt',
+        'fileB': SourceFileRoot / 'dir2' / 'fileB.txt'
+    },
+    handles = {}
+)
+
 @fixture
-def copiedtestfiles(tmp_path) -> Testfiles:
-    dir1.copy(tmp_path)
-    dir2.copy(tmp_path)
+def copiedtestfiles(request, tmp_path) -> Testfiles:
     tmp_files = Testfiles(
         root = tmp_path,
-        paths = {
-            'dir1': Path(tmp_path / 'dir1'),
-            'dir2': Path(tmp_path / 'dir2'),
-            'fileA': Path(tmp_path / 'dir1' / 'fileA.txt'),
-            'fileB': Path(tmp_path / 'dir2' / 'fileB.txt')
-        },
-        handles = {}
+        paths = defaultdict(list),
+        handles = defaultdict(list)
     )
-    return tmp_files
+
+    filestocopy = request.node.get_closest_marker('copyfiles').args
+    for file in filestocopy:
+        fileid, numcopies = file
+        for i in range(numcopies):
+            uniquedir = tmp_path / str(uuid.uuid1())
+            uniquedir.mkdir()
+            sourcefiles.paths[fileid].copy(tmp_path / uniquedir)
+            tmp_files.paths[fileid].append(tmp_path / uniquedir / sourcefiles.paths[fileid].name)
+            assert len(tmp_files.paths[fileid]) == i + 1
+    yield tmp_files
+
+@fixture
+def filesopen(copiedtestfiles):
+    with ExitStack() as stack:
+        copiedtestfiles.handles = {
+            fileid: [stack.enter_context(open(path, 'rb')) for path in paths]
+            for fileid, paths in copiedtestfiles.paths.items()
+        }
+        yield
 
 @fixture
 def duplicatedir1(copiedtestfiles) -> Testfiles:
