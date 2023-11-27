b30a1df1698fc37f77bcedd34e6a22d97f2cb032
Mike Foster <i.am.mike.foster@gmail.com>
2023-11-27 15:41:32 +0100

    cleaner fixtures working for all test_filedetails.py

--- test/majorver/test_filedetails.py
+++ test/majorver/test_filedetails.py
@@ -3,25 +3,28 @@ from recurtools import flatten
 from . import listfiles
 from .testimports import *
 
+@mark.copyfiles(('fileA',1), ('fileB',1))
 def test_fileslisted(copiedtestfiles):
     filesdict = listfiles(copiedtestfiles.root)
     files = [file for file in flatten(filesdict.values())]
     assert len(files) == 2
-    assert copiedtestfiles.paths['fileA'] in files
-    assert copiedtestfiles.paths['fileB'] in files
+    assert copiedtestfiles.paths['fileA'][0] in files
+    assert copiedtestfiles.paths['fileB'][0] in files
 
+@mark.copyfiles(('fileA',1), ('fileB',1))
 def test_filesindexedbysize(copiedtestfiles):
     filesdict = listfiles(copiedtestfiles.root)
     assert filesdict == {
-        16: {copiedtestfiles.paths['fileA']},
-        23: {copiedtestfiles.paths['fileB']}
+        16: {copiedtestfiles.paths['fileA'][0]},
+        23: {copiedtestfiles.paths['fileB'][0]}
     }
 
-def test_filesofsamesize(copiedtestfiles, duplicatedir1):
+@mark.copyfiles(('fileA', 2), ('fileB',1))
+def test_filesofsamesize(copiedtestfiles):
     filesdict = listfiles(copiedtestfiles.root)
     assert len(filesdict) == 2
     assert filesdict[16] == {
-        copiedtestfiles.paths['fileA'],
-        copiedtestfiles.paths['fileA-copy']
+        copiedtestfiles.paths['fileA'][0],
+        copiedtestfiles.paths['fileA'][1]
     }
-    assert filesdict[23] == {copiedtestfiles.paths['fileB']}
+    assert filesdict[23] == {copiedtestfiles.paths['fileB'][0]}
