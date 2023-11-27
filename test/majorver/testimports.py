a8ceff5d884251644c11eb6b963d7e06f7c54e75
Mike Foster <i.am.mike.foster@gmail.com>
2023-11-27 15:44:44 +0100

    tidy up imports

--- test/majorver/testimports.py
+++ test/majorver/testimports.py
@@ -1,13 +1,15 @@
+import uuid
 from collections import defaultdict
 from contextlib import ExitStack
 from dataclasses import dataclass
 from io import BufferedIOBase
 from pathlib import Path
-import uuid
+
 from pytest import fixture, mark, raises
 
+
 def _copy(self: Path, target: Path) -> None:
-    from shutil import copytree, copyfile
+    from shutil import copyfile, copytree
     if self.is_dir():
         copytree(self, target / self.name)
     else:
@@ -69,48 +71,4 @@ def filesopen(copiedtestfiles):
             fileid: [stack.enter_context(open(path, 'rb')) for path in paths]
             for fileid, paths in copiedtestfiles.paths.items()
         }
-        yield
-
-@fixture
-def duplicatedir1(copiedtestfiles) -> Testfiles:
-    dir1.copy(copiedtestfiles.root / 'alt')
-    copiedtestfiles.paths['dir1-copy'] = copiedtestfiles.root / 'alt' 
-    copiedtestfiles.paths['fileA-copy'] = copiedtestfiles.root / 'alt' / 'dir1' / 'fileA.txt'
-    return copiedtestfiles
-
-@fixture
-def twocopiesfileAopen(duplicatedir1) -> Testfiles:
-    with duplicatedir1.paths['fileA'].open('rb') as handle1, duplicatedir1.paths['fileA-copy'].open('rb') as handle2:
-        duplicatedir1.handles['fileA'] = handle1
-        duplicatedir1.handles['fileA-copy'] = handle2
-        yield duplicatedir1
-
-@fixture
-def fileAopened(copiedtestfiles):
-    with copiedtestfiles.paths['fileA'].open('rb') as filehandle:
-        copiedtestfiles.handles['fileA'] = filehandle
-        yield
-
-@fixture
-def fileBopened(copiedtestfiles):
-    with copiedtestfiles.paths['fileB'].open('rb') as filehandle:
-        copiedtestfiles.handles['fileB'] = filehandle
-        yield
-
-@fixture
-def fileAcopyopened(copiedtestfiles):
-    with copiedtestfiles.paths['fileA-copy'].open('rb') as filehandle:
-        copiedtestfiles.handles['fileA-copy'] = filehandle
-        yield
-
-@fixture
-def fileAhardlinked(copiedtestfiles):
-    copiedtestfiles.paths['fileA-link'] = copiedtestfiles.root / 'fileAlink.txt'
-    copiedtestfiles.paths['fileA-link'].hardlink_to(copiedtestfiles.paths['fileA'])
-    return copiedtestfiles
-
-@fixture
-def fileAlinkopened(copiedtestfiles):
-    with copiedtestfiles.paths['fileA-link'].open('rb') as filehandle:
-        copiedtestfiles.handles['fileA-link'] = filehandle
         yield
