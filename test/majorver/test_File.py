9f1c3ef972f08e0e1e3e2359ab277917df40cfd7
Mike Foster <i.am.mike.foster@gmail.com>
2023-11-27 15:39:34 +0100

    cleaner fixtures working for all test_File.py

--- test/majorver/test_File.py
+++ test/majorver/test_File.py
@@ -1,52 +1,60 @@
 from . import BufferedIOFile
 from .testimports import *
 
-def test_initialisation(copiedtestfiles, fileAopened):
-    testfile = BufferedIOFile(copiedtestfiles.paths['fileA'], copiedtestfiles.handles['fileA'])
-    assert testfile.path == copiedtestfiles.paths['fileA']
-    assert testfile.handle == copiedtestfiles.handles['fileA']
+@mark.copyfiles(('fileA',1))
+def test_initialisation(copiedtestfiles, filesopen):
+    testfile = BufferedIOFile(copiedtestfiles.paths['fileA'][0], copiedtestfiles.handles['fileA'][0])
+    assert testfile.path == copiedtestfiles.paths['fileA'][0]
+    assert testfile.handle == copiedtestfiles.handles['fileA'][0]
 
-def test_immutability(copiedtestfiles, fileAopened, fileBopened):
-    testfile = BufferedIOFile(copiedtestfiles.paths['fileA'], copiedtestfiles.handles['fileA'])
+@mark.copyfiles(('fileA',1), ('fileB', 1))
+def test_immutability(copiedtestfiles, filesopen):
+    testfile = BufferedIOFile(copiedtestfiles.paths['fileA'][0], copiedtestfiles.handles['fileA'][0])
     with raises(AttributeError):
         testfile.path = Path('dir1')
     with raises(AttributeError):
-        testfile.handle = copiedtestfiles.handles['fileB']
+        testfile.handle = copiedtestfiles.handles['fileB'][0]
 
-def test_hashable(copiedtestfiles, fileAopened, fileBopened):
-    testfileA = BufferedIOFile(copiedtestfiles.paths['fileA'], copiedtestfiles.handles['fileA'])
-    testfileB = BufferedIOFile(copiedtestfiles.paths['fileB'], copiedtestfiles.handles['fileB'])
+@mark.copyfiles(('fileA',1), ('fileB', 1))
+def test_hashable(copiedtestfiles, filesopen):
+    testfileA = BufferedIOFile(copiedtestfiles.paths['fileA'][0], copiedtestfiles.handles['fileA'][0])
+    testfileB = BufferedIOFile(copiedtestfiles.paths['fileB'][0], copiedtestfiles.handles['fileB'][0])
     files = {testfileA, testfileB}
     assert files
 
-def test_uniquenessbasedonpath(copiedtestfiles, fileAopened, fileBopened):
-    testfileA = BufferedIOFile(copiedtestfiles.paths['fileA'], copiedtestfiles.handles['fileA'])
-    testfileB = BufferedIOFile(copiedtestfiles.paths['fileB'], copiedtestfiles.handles['fileB'])
-    with copiedtestfiles.paths['fileA'].open() as fileA2Handle:
-        testfileA2 = BufferedIOFile(copiedtestfiles.paths['fileA'], fileA2Handle)
+@mark.copyfiles(('fileA',1), ('fileB', 1))
+def test_uniquenessbasedonpath(copiedtestfiles, filesopen):
+    testfileA = BufferedIOFile(copiedtestfiles.paths['fileA'][0], copiedtestfiles.handles['fileA'][0])
+    testfileB = BufferedIOFile(copiedtestfiles.paths['fileB'][0], copiedtestfiles.handles['fileB'][0])
+    with copiedtestfiles.paths['fileA'][0].open() as fileA2Handle:
+        testfileA2 = BufferedIOFile(copiedtestfiles.paths['fileA'][0], fileA2Handle)
     assert testfileA == testfileA2
-    assert testfileA == copiedtestfiles.paths['fileA']
+    assert testfileA == copiedtestfiles.paths['fileA'][0]
     files = {testfileA, testfileB, testfileA2}
     assert len(files) == 2
     assert files == {testfileA, testfileB} == {testfileA2, testfileB}
 
-def test_readbychunk(copiedtestfiles, fileAopened):
-    testfile = BufferedIOFile(copiedtestfiles.paths['fileA'], copiedtestfiles.handles['fileA'], chunksize=4)
+@mark.copyfiles(('fileA',1))
+def test_readbychunk(copiedtestfiles, filesopen):
+    testfile = BufferedIOFile(copiedtestfiles.paths['fileA'][0], copiedtestfiles.handles['fileA'][0], chunksize=4)
     contents = [chunk for chunk in testfile]
     assert contents == [b'some', b' ran', b'dom ', b'text']
 
-def test_notdivisiblebychunksize(copiedtestfiles, fileBopened):
-    testfile = BufferedIOFile(copiedtestfiles.paths['fileB'], copiedtestfiles.handles['fileB'], chunksize=16)
+@mark.copyfiles(('fileB',1))
+def test_notdivisiblebychunksize(copiedtestfiles, filesopen):
+    testfile = BufferedIOFile(copiedtestfiles.paths['fileB'][0], copiedtestfiles.handles['fileB'][0], chunksize=16)
     contents = [chunk for chunk in testfile]
     assert contents == [b'some longer rand', b'om text']
 
-def test_defaultchunksize(copiedtestfiles, fileAopened):
-    testfile = BufferedIOFile(copiedtestfiles.paths['fileA'], copiedtestfiles.handles['fileA'])
+@mark.copyfiles(('fileA',1))
+def test_defaultchunksize(copiedtestfiles, filesopen):
+    testfile = BufferedIOFile(copiedtestfiles.paths['fileA'][0], copiedtestfiles.handles['fileA'][0])
     contents = [chunk for chunk in testfile]
     assert contents == [b'some random text']
 
-def test_readchunk(copiedtestfiles, fileBopened):
-    testfile = BufferedIOFile(copiedtestfiles.paths['fileB'], copiedtestfiles.handles['fileB'], chunksize=16)
+@mark.copyfiles(('fileB',1))
+def test_readchunk(copiedtestfiles, filesopen):
+    testfile = BufferedIOFile(copiedtestfiles.paths['fileB'][0], copiedtestfiles.handles['fileB'][0], chunksize=16)
     chunk = testfile.readchunk()
     assert chunk == b'some longer rand'
     chunk = testfile.readchunk()
