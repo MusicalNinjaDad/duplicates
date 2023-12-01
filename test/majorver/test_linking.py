31fe487c81a2ce4c9d207b252e1d998fd32d1b6e
Mike Foster <i.am.mike.foster@gmail.com>
2023-12-01 19:07:21 +0100

    improved test

--- test/majorver/test_linking.py
+++ test/majorver/test_linking.py
@@ -6,8 +6,14 @@ def test_replacecopywithlink(copiedtestfiles):
     replacewithlink(copiedtestfiles.paths['fileA'][0], copiedtestfiles.paths['fileA'][1])
     assert copiedtestfiles.paths['fileA'][0].stat().st_ino == copiedtestfiles.paths['fileA'][1].stat().st_ino
 
-@mark.copyfiles(('fileA',2))
+@mark.copyfiles(('fileA',2),('fileB',3))
 def test_findandreplacecopywithlinks(copiedtestfiles):
-    assert copiedtestfiles.paths['fileA'][0].stat().st_ino != copiedtestfiles.paths['fileA'][1].stat().st_ino
     linkdupes(copiedtestfiles.root)
-    assert copiedtestfiles.paths['fileA'][0].stat().st_ino == copiedtestfiles.paths['fileA'][1].stat().st_ino
+    fileAino = copiedtestfiles.paths['fileA'][0].stat().st_ino
+    fileBino = copiedtestfiles.paths['fileB'][0].stat().st_ino
+    assert all(
+        file.stat().st_ino == fileAino for file in copiedtestfiles.paths['fileA']
+    )
+    assert all(
+        file.stat().st_ino == fileBino for file in copiedtestfiles.paths['fileB']
+    )
