7aa13f8dac721538eafa528ae0a50558ca15136a
Mike Foster <i.am.mike.foster@gmail.com>
2023-12-07 18:22:48 +0100

    fix the test case to fail for right reasons

--- test/minorver/test_DuplicateFiles_internals.py
+++ test/minorver/test_DuplicateFiles_internals.py
@@ -7,16 +7,16 @@ def test_inoindex(copiedtestfiles):
     duplicatefiles = DuplicateFiles.frompath(copiedtestfiles.root)
     assert duplicatefiles._inoindex == {
         copiedtestfiles.paths['fileA'][0].stat().st_ino: frozenset({
-            copiedtestfiles['fileA'][0].path,
-            copiedtestfiles['fileA'][2].path
+            copiedtestfiles.paths['fileA'][0],
+            copiedtestfiles.paths['fileA'][2]
         }),
         copiedtestfiles.paths['fileA'][1].stat().st_ino: frozenset({
-            copiedtestfiles.paths['fileA'][1].path
+            copiedtestfiles.paths['fileA'][1]
         }),
         # copiedtestfiles.paths['fileB'][0].stat().st_ino: frozenset({
-        #     copiedtestfiles.paths['fileB'][0].path
+        #     copiedtestfiles.paths['fileB'][0]
         # }), -- Not in index as only file of this size => cannot be a duplicate
         copiedtestfiles.paths['fileA2'][0].stat().st_ino: frozenset({
-            copiedtestfiles.paths['fileA2'][0].path
+            copiedtestfiles.paths['fileA2'][0]
         })
     }
