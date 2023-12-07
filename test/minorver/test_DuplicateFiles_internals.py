118c1ef5bafe94588172e66ab9b96e7930c7a073
Mike Foster <i.am.mike.foster@gmail.com>
2023-12-07 18:10:16 +0100

    red: build and store inoindex in DuplicateFiles

---
+++ test/minorver/test_DuplicateFiles_internals.py
@@ -0,0 +1,22 @@
+from . import *
+
+@mark.xfail(reason = 'Not Implemented')
+@mark.copyfiles(('fileA',2), ('fileB', 1), ('fileA2', 1))
+@mark.linkfiles(('fileA',1))
+def test_inoindex(copiedtestfiles):
+    duplicatefiles = DuplicateFiles.frompath(copiedtestfiles.root)
+    assert duplicatefiles._inoindex == {
+        copiedtestfiles.paths['fileA'][0].stat().st_ino: frozenset({
+            BufferedIOFile(copiedtestfiles['fileA'][0]),
+            BufferedIOFile(copiedtestfiles['fileA'][2])
+        }),
+        copiedtestfiles.paths['fileA'][1].stat().st_ino: frozenset({
+            BufferedIOFile(copiedtestfiles.paths['fileA'][1])
+        }),
+        # copiedtestfiles.paths['fileB'][0].stat().st_ino: frozenset({
+        #     BufferedIOFile(copiedtestfiles.paths['fileB'][0])
+        # }), -- Not in index as only file of this size => cannot be a duplicate
+        copiedtestfiles.paths['fileA2'][0].stat().st_ino: frozenset({
+            BufferedIOFile(copiedtestfiles.paths['fileA2'][0])
+        })
+    }
