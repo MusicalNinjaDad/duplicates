2d12a3202286d4fd746c5a06e000fae8113fcc92
Mike Foster <i.am.mike.foster@gmail.com>
2023-12-08 19:12:36 +0100

    follow pattern for Ignoresamenames

--- test/majorver/test_output.py
+++ test/majorver/test_output.py
@@ -8,6 +8,17 @@ from . import *
 SEPARATOR = '\n=====================\n'
 #separator is part of commited interface as others may choose to parse this output
 
+@fixture(scope='class', autouse=True)
+def setup(request, classtestfiles):
+    testclass = request.cls
+    files = flatten(testclass.filegroups.values(), preservestrings=True)
+    testclass.filepaths = [str(path) for file in files for path in classtestfiles.paths[file]]
+
+    returnedstring = testclass.act(classtestfiles)
+
+    testclass.stringblocks = returnedstring.split(SEPARATOR)
+    testclass.stringelements = [s.split('\n') for s in testclass.stringblocks]
+
 @mark.copyfiles(('fileA',2), ('fileA2',2), ('fileB',3))
 class TestPrintoutcontents:
     files = [
@@ -16,17 +27,10 @@ class TestPrintoutcontents:
             'fileB'
         ]
 
-    @fixture(scope='class', autouse=True)
-    def setup(self, request, classtestfiles):
-        files = request.cls.files
-        request.cls.filepaths = [str(path) for file in files for path in classtestfiles.paths[file]]
-
-        dupes = {frozenset(BufferedIOFile(path) for path in filepaths) for filepaths in classtestfiles.paths.values()}
+    def act(testfiles):
+        dupes = {frozenset(BufferedIOFile(path) for path in filepaths) for filepaths in testfiles.paths.values()}
         duplicatefiles = DuplicateFiles(duplicates=dupes, inoindex=None)
-        returnedstring = duplicatefiles.printout()
-
-        request.cls.stringblocks = returnedstring.split(SEPARATOR)
-        request.cls.stringelements = [s.split('\n') for s in self.stringblocks]
+        return duplicatefiles.printout()
 
     def test_correctnumberofblocks(self):
         assert len(self.stringblocks) == 3
@@ -45,6 +49,7 @@ class TestPrintoutcontents:
         filepaths = [list(p) for p in permutations([str(path) for path in classtestfiles.paths[fileid]])]
         assert any([groupofpaths in filepaths for groupofpaths in self.stringelements]), f'{fileid} path group not found'
 
+@mark.xfail
 @mark.copyfiles(('fileA',2), ('fileA2',2), ('fileB',3), ('fileA-copy',1))
 def test_printoutcontents_frompath(copiedtestfiles):
     files = [
@@ -79,24 +84,30 @@ def test_printoutcontents_frompath(copiedtestfiles):
     assert any([groupofpaths in fileA2paths for groupofpaths in stringelements]), 'fileA2 path group not found'
 
 @mark.copyfiles(('fileA',2), ('fileA2',2), ('fileB',3), ('fileA-copy',1))
-def test_printoutcontents_ignoresamenames(copiedtestfiles):
-    files = [
-        'fileA',
-        'fileA-copy'
-    ]
+class TestPrintoutcontents_Ignoresamenames():
+    filegroups = {
+        'fileA': ('fileA','fileA-copy')
+    }
 
-    filepaths = [str(path) for file in files for path in copiedtestfiles.paths[file]]
+    def act(testfiles):
+        duplicatefiles = DuplicateFiles.frompath(testfiles.root)
+        return duplicatefiles.printout(ignoresamenames=True)
 
-    duplicatefiles = DuplicateFiles.frompath(copiedtestfiles.root)
-    returnedstring = duplicatefiles.printout(ignoresamenames=True)
-
-    stringblocks = returnedstring.split(SEPARATOR)
-    assert len(stringblocks) == 1
+    def test_correctnumberofblocks(self):
+        assert len(self.stringblocks) == 1
     
-    stringelements = [s.split('\n') for s in stringblocks]
-    assert all([path in flatten(stringelements, preservestrings=True) for path in filepaths]), f'Some paths missing in output: {stringelements}'
-    assert all([path in filepaths for path in flatten(stringelements, preservestrings=True)]), f'Additional paths found in output: {stringelements}'
+    def test_correctpaths(self):  
+        assert all([path in flatten(self.stringelements, preservestrings=True) for path in self.filepaths]), 'Some paths missing in output'
+        assert all([path in self.filepaths for path in flatten(self.stringelements, preservestrings=True)]), 'Additional paths found in output'
 
-    fileApaths = [list(p) for p in permutations([str(path) for path in copiedtestfiles.paths['fileA'] + copiedtestfiles.paths['fileA-copy']])]
-    assert any([permutation in stringelements for permutation in fileApaths]), f'fileA paths not properly grouped: {stringelements}'
-    assert any([groupofpaths in fileApaths for groupofpaths in stringelements]), f'fileA path group not found: {stringelements}'
+    @mark.parametrize('filegroup', filegroups.values(), ids=filegroups)
+    def test_pathsgrouped(self, filegroup, classtestfiles):
+        groupedpaths = [p for fileid in filegroup for p in classtestfiles.paths[fileid]]
+        filepaths = [list(permutation) for permutation in permutations([str(path) for path in groupedpaths])]
+        assert any([permutation in self.stringelements for permutation in filepaths]), f'{filegroup} paths not properly grouped'
+
+    @mark.parametrize('filegroup', filegroups.values(), ids=filegroups)
+    def test_pathgroupexists(self, filegroup, classtestfiles):
+        groupedpaths = [p for fileid in filegroup for p in classtestfiles.paths[fileid]]
+        filepaths = [list(permutation) for permutation in permutations([str(path) for path in groupedpaths])]
+        assert any([groupofpaths in filepaths for groupofpaths in self.stringelements]), f'{filegroup} path group not found. Expected: {groupedpaths}, got: {self.stringelements}'
