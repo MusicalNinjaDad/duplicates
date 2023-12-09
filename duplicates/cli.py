050a111b507252604bb7812ebbdee6b0e13ea158
Mike Foster <i.am.mike.foster@gmail.com>
2023-12-09 19:43:58 +0100

    link files via command line

--- duplicates/cli.py
+++ duplicates/cli.py
@@ -1,9 +1,14 @@
+from pathlib import Path
 from click import argument, command, option
+
+from . import DuplicateFiles
 
 
 @command()
 @argument("rootdir")
 @option("--link", is_flag=True)
 def dupes(rootdir, link):
+    duplicatefiles = DuplicateFiles.frompath(Path(rootdir))
     if link:
         print(f'I will link files in {rootdir}')
+        duplicatefiles.link()
--- test/minorver/test_cli.py
+++ test/minorver/test_cli.py
@@ -1,19 +1,17 @@
-from subprocess import run
-from . import *
-
-pytestmark = mark.tox
+import os
+from pytest import mark
+from ...duplicates.cli import dupes
+from click.testing import CliRunner
 
 @mark.copyfiles(('fileA',2),('fileB',3))
 def test_link(copiedtestfiles):
+    clirunner = CliRunner()
     command = [
-        'dupes',
         '--link',
         os.fspath(copiedtestfiles.root)
     ]
-    
-    completed = run(command, capture_output=True)
-
-    assert completed.stdout.decode().strip() == f'I will link files in {copiedtestfiles.root}'
+    result = clirunner.invoke(dupes, command)
+    assert result.output.strip() == f'I will link files in {copiedtestfiles.root}'
 
     fileAino = copiedtestfiles.paths['fileA'][0].stat().st_ino
     fileBino = copiedtestfiles.paths['fileB'][0].stat().st_ino
