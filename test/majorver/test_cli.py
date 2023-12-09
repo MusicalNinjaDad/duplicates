d87553c8bdad12499348757a4c6af890e9c5e0c2
Mike Foster <i.am.mike.foster@gmail.com>
2023-12-09 15:35:52 +0100

    get tox up and running

---
+++ test/majorver/test_cli.py
@@ -0,0 +1,23 @@
+from subprocess import run
+from . import *
+
+pytestmark = mark.tox
+
+@mark.copyfiles(('fileA',2),('fileB',3))
+def test_link(copiedtestfiles):
+    command = [
+        'dupes',
+        '--link',
+        copiedtestfiles.root
+    ]
+    
+    run(command)
+
+    fileAino = copiedtestfiles.paths['fileA'][0].stat().st_ino
+    fileBino = copiedtestfiles.paths['fileB'][0].stat().st_ino
+    assert all(
+        file.stat().st_ino == fileAino for file in copiedtestfiles.paths['fileA']
+    )
+    assert all(
+        file.stat().st_ino == fileBino for file in copiedtestfiles.paths['fileB']
+    )
