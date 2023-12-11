c35230a34bd8e97775a331ca4cf15a2028708478
Mike Foster <i.am.mike.foster@gmail.com>
2023-12-11 15:40:57 +0100

    remove copy paste error in tests

--- test/majorver/test_dupes_identification.py
+++ test/majorver/test_dupes_identification.py
@@ -7,12 +7,12 @@ def test_comparefilecontents(copiedtestfiles, filesopen):
         (copiedtestfiles.handles['fileA'] + copiedtestfiles.handles['fileA2'])
     )
     filestocompare = {frozenset(
-        BufferedIOFile(path_handle[0], path_handle[1], chunksize = 4) for path_handle in pathsandhandles
+        BufferedIOFile(path_handle[0], path_handle[1]) for path_handle in pathsandhandles
     )}
     identicalfiles = comparefilecontents(filestocompare)
     assert identicalfiles == {
-        frozenset(BufferedIOFile(path, chunksize = 4) for path in copiedtestfiles.paths['fileA']),
-        frozenset(BufferedIOFile(path, chunksize = 4) for path in copiedtestfiles.paths['fileA2'])
+        frozenset(BufferedIOFile(path) for path in copiedtestfiles.paths['fileA']),
+        frozenset(BufferedIOFile(path) for path in copiedtestfiles.paths['fileA2'])
     }
 
 @mark.copyfiles(('fileA',2), ('fileA2',1), ('fileB', 4))
@@ -22,12 +22,12 @@ def test_comparefilecontents_filehasnoduplicate(copiedtestfiles, filesopen):
         (copiedtestfiles.handles['fileA'] + copiedtestfiles.handles['fileA2'] + copiedtestfiles.handles['fileB'])
     )
     filestocompare = {frozenset(
-        BufferedIOFile(path_handle[0], path_handle[1], chunksize = 4) for path_handle in pathsandhandles
+        BufferedIOFile(path_handle[0], path_handle[1]) for path_handle in pathsandhandles
     )}
     identicalfiles = comparefilecontents(filestocompare)
     assert identicalfiles == {
-        frozenset(BufferedIOFile(path, chunksize = 4) for path in copiedtestfiles.paths['fileA']),
-        frozenset(BufferedIOFile(path, chunksize = 4) for path in copiedtestfiles.paths['fileB'])
+        frozenset(BufferedIOFile(path) for path in copiedtestfiles.paths['fileA']),
+        frozenset(BufferedIOFile(path) for path in copiedtestfiles.paths['fileB'])
     }
 
 @mark.copyfiles(('fileA',2), ('fileB', 1), ('fileA2', 1))
@@ -36,21 +36,22 @@ def test_instantiatefrompath(copiedtestfiles):
     duplicatefiles = DuplicateFiles.frompath(copiedtestfiles.root)
     assert any((
         duplicatefiles.duplicates == {frozenset((
-                                BufferedIOFile(copiedtestfiles.paths['fileA'][1], chunksize=4),
-                                BufferedIOFile(copiedtestfiles.paths['fileA'][0], chunksize=4)
+                                BufferedIOFile(copiedtestfiles.paths['fileA'][1]),
+                                BufferedIOFile(copiedtestfiles.paths['fileA'][0])
                                 ))
                             },
         duplicatefiles.duplicates == {frozenset((
-                                BufferedIOFile(copiedtestfiles.paths['fileA'][1], chunksize=4),
-                                BufferedIOFile(copiedtestfiles.paths['fileA'][2], chunksize=4)
+                                BufferedIOFile(copiedtestfiles.paths['fileA'][1]),
+                                BufferedIOFile(copiedtestfiles.paths['fileA'][2])
                                 ))
                             }
     )), f'Following files identified as duplicates: {duplicatefiles.duplicates}'
+
 
 @mark.copyfiles(('fileA',2), ('fileA2',1), ('fileB', 4))
 def test_instantiatefrompath_multipleduplicatefiles(copiedtestfiles):
     identicalfiles = DuplicateFiles.frompath(copiedtestfiles.root)
     assert identicalfiles.duplicates == {
-        frozenset(BufferedIOFile(path, chunksize = 4) for path in copiedtestfiles.paths['fileA']),
-        frozenset(BufferedIOFile(path, chunksize = 4) for path in copiedtestfiles.paths['fileB'])
+        frozenset(BufferedIOFile(path) for path in copiedtestfiles.paths['fileA']),
+        frozenset(BufferedIOFile(path) for path in copiedtestfiles.paths['fileB'])
     }
